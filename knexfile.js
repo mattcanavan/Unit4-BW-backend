9fc2e70591848c61cbeaf48f3d2474f202b82ddc
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-12-19 17:06:33 -0800

    Defined knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './database/users.db3',
+      filename: './database/project-database.db3',
     },
     pool: {
       afterCreate: (conn, done) => {
