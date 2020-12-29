2be705122e62ace9e785284b0bb0039bb275e0a6
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-12-29 13:48:05 -0800

    Removed user_types tlb and truck owner column. Added user_favorites tbl.

--- database/migrations/20201219162040_project-database.js
+++ database/migrations/20201219162040_project-database.js
@@ -1,69 +1,67 @@
 
 exports.up = function(knex) {
     return knex.schema
-    .createTable('users_types', tbl => {
-        tbl.increments('user_type_id')
-        tbl.text('user_type')
-    })
 
     .createTable('users', tbl => {
-          tbl.increments('user_id')
+          tbl.increments('user_id').primary()
           tbl.text('username', 128).notNullable().unique()
           tbl.text('password', 256).notNullable()
           tbl.text('email', 128).notNullable()
-          tbl.integer('user_type_id').unsigned().notNullable().defaultTo(1)   //diner, lowest permissions.
-          .references('user_type_id').inTable('users_types')
-          .onDelete('RESTRICT').onUpdate('RESTRICT')
           tbl.decimal('current_location_lat', 128)
           tbl.decimal('current_location_long', 128)
           tbl.timestamps(true, true)    // Adds created_at and updated_at columns on the database, setting each to datetime types. table.timestamps([useTimestamps], [defaultToNow])
-        })
+    })
         
     .createTable('trucks', tbl => {
-        tbl.increments('truck_id')
+        tbl.increments('truck_id').primary()
         tbl.text('truck_name').notNullable().unique()
-        tbl.integer('truck_owner_id').unsigned().notNullable()
-        .references('user_id').inTable('users')
-        .onDelete('RESTRICT').onUpdate('RESTRICT')
         tbl.text('truck_img_url', 128)
         tbl.text('cusine_type', 128).notNullable()
         tbl.decimal('current_location_lat', 128).notNullable()
         tbl.decimal('current_location_long', 128).notNullable()
-        tbl.timestamps(true, true)    // Adds created_at and updated_at columns on the database, setting each to datetime types. table.timestamps([useTimestamps], [defaultToNow])
-  })
+    })
 
     .createTable('menus', tbl => {
-        tbl.increments('item_id')
+        tbl.increments('item_id').primary()
         tbl.integer('truck_id').unsigned().notNullable()
         .references('truck_id').inTable('trucks')
-        .onDelete('RESTRICT').onUpdate('RESTRICT')  //might allow unrestricted updating later
+        .onUpdate('CASCADE').onDelete('CASCADE')
         tbl.text('item_name', 128).notNullable()
-        tbl.text('item_description', 128)    //might make nullable later
-        tbl.text('item_img_url', 128)   //might make nullable later or add default url?
+        tbl.text('item_description', 128)                       //might make nullable later
+        tbl.text('item_img_url', 128)                           //might make nullable later or add default url?
         tbl.decimal('item_price').notNullable()
     })
 
     .createTable('trucks_reviews', tbl => {
-        tbl.increments('review_id')
+        tbl.increments('review_id').primary()
         tbl.integer('truck_id').unsigned().notNullable()
         .references('truck_id').inTable('trucks')
-        .onDelete('RESTRICT').onUpdate('RESTRICT')  //might allow unrestricted updating later
+        .onUpdate('CASCADE').onDelete('CASCADE')
         tbl.integer('user_id').unsigned().notNullable()
         .references('user_id').inTable('users')
-        .onDelete('RESTRICT').onUpdate('RESTRICT')
+        .onUpdate('CASCADE').onDelete('CASCADE')
         tbl.integer('food_quality_star_count').notNullable()
         tbl.integer('service_star_count').notNullable()
         tbl.integer('cleanliness_star_count').notNullable()
         tbl.text('comment', 228)
     })
+
+    .createTable('user_favorites', tbl => {
+        tbl.increments('id').primary()
+        tbl.integer('user_id').unsigned().notNullable()
+        .references('user_id').inTable('users')
+        .onUpdate('CASCADE').onDelete('CASCADE')
+        tbl.integer('truck_id').unsigned().notNullable()
+        .references('truck_id').inTable('trucks')
+        .onUpdate('CASCADE').onDelete('CASCADE')
+    })
 };
 
 exports.down = function(knex) {
     return knex.schema
-    .dropTableIfExists('users_types')
-    .dropTableIfExists('users')
-    .dropTableIfExists('trucks')
-    .dropTableIfExists('menus')
+    .dropTableIfExists('user_favorites')
     .dropTableIfExists('trucks_reviews')
-  
+    .dropTableIfExists('menus')
+    .dropTableIfExists('trucks')
+    .dropTableIfExists('users')
 };
